aafdc3b6faae2d00314d08f3589917690da9cbd8
chessjeff <122488700+chessjeff@users.noreply.github.com>
2024-06-09 22:32:44 -0600

    Displays game pieces after game over

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const Board = (function() {
     const writeToBoard = function(selection, mark) {
         const j = parseInt(selection.split('')[0]),
             k = parseInt(selection.split('')[2]);
-        if (Board.boardArray[j][k]) {
+        if (j > 2 || k > 2) {
+            alert("invalid choice")
+            Game.turn(mark);
+        } else if (Board.boardArray[j][k]) {
             alert("This space has already been selected")
-            Game.turn();
+            Game.turn(mark);
+        } else {
+            Board.boardArray[j].splice(k, 1, mark)
+            //Display logic branches from game logic here
+            Display.displayBoard(j, k, mark)
         }
-        Board.boardArray[j].splice(k, 1, mark)
     }
 
     let checkedArr = []
@@ -19,7 +25,8 @@ const Board = (function() {
                     Board.checkedArr.push(b);
                 }
             }
-            let result = Board.checkedArr.length > 8; //eventually substitute with variable so any size board is possible
+            console.log(Board.checkedArr)
+            let result = Board.checkedArr.length >= 9; //eventually substitute with variable so any size board is possible
             return result; // Bool
         }
     }
@@ -28,7 +35,11 @@ const Board = (function() {
         return Board.boardArray = [['','',''],['','',''],['','','']];
     }
 
-    return {boardArray, writeToBoard, checkedArr, checkFullBoard, clearBoard}
+    const clearCheck = function() {
+        return Board.checkedArr = [];
+    }
+
+    return {boardArray, writeToBoard, checkedArr, checkFullBoard, clearBoard, clearCheck}
 })();
 
 const Players = (function() {
@@ -46,12 +57,11 @@ const Players = (function() {
 
     //cycles player turns by cycling through an array
     let i = 0;
-    const markerIndex = () => [playerList[0].marker, playerList[1].marker];
     const alternateTurns = function() {
         let mark;
         if (playerList.length == 2) {
-            i == 0 ? i++ : i--;
-            mark = markerIndex()[i];
+            i == 1 ? i-- : i++;
+            mark = playerList[i].marker;
             return mark;
         }
     }
@@ -70,21 +80,107 @@ const Game = (function() {
         Players.createPlayer(prompt("Enter Player 2 Name"))
         console.log(Players.playerList)
         //while prompts players until game win or full board
-        while (Board.checkFullBoard) {
+        while (!checkWin(Board.boardArray)) {
             let mark = Players.alternateTurns()
             //incase player chooses already chosen spot, doesn't skip their turn
             Game.turn(mark);
+            if (Game.checkWin(Board.boardArray)) {
+                alert(`Game Over! ${mark} won!`)
+                break;
+            } else if (Board.checkFullBoard()) {
+                alert('Tie Game!')
+                break;
+            }
         }
+        alert("Please reset the game")
     }
 
     const turn = function(mark) {
         let selection = Game.getSelection() //string
-        console.log({selection})
         Board.writeToBoard(selection, mark) //split str into 2 nums. place mark in spot
         console.log(Board.boardArray)
     }
 
-    const getSelection = () => prompt("Enter Cell (j, k)")
+    const getSelection = () => prompt("Enter Cell (j,k)")
 
-    return {main, turn, getSelection}
+    const resetGame = function() {
+        Board.clearBoard()
+        Board.clearCheck()
+        Players.clearPlayerList()
+    }
+
+    const checkWin = function(arr) {
+        const getCols = (function() {
+            let cols = [];
+            const col = (arr, n) => arr.map(x => x[n])
+            for (let i = 0; i < arr.length; i++) {
+                cols.push(col(arr, i));
+            } 
+            return cols
+        })();
+
+        const getDiags = (function() {
+            let posDiag = [],
+                negDiag = [],
+                step = 0;
+            for (let i = 0; i < arr.length; i++) {
+                negDiag.push(arr[i][step])
+                step++;
+            }
+            step = 0;
+            for (let j = arr.length - 1; j >=0; j--) {
+                posDiag.push(arr[j][step]);
+                step++;
+            }
+            let diags = [negDiag, posDiag]
+            return diags
+        })();
+
+        let gameWon = false;
+        const checkFalsy = (e) => e === false;
+        const findWin = function(arr) {
+            let exit = false;
+            arr.forEach(i => {
+                if (!exit) {
+                    let inner = []; // track booleans
+                    i.forEach(j => {i[0] !== ''
+                        ? j === i[0] 
+                            ? inner.push(true) 
+                            : inner.push(false)
+                        : inner.push(false);
+                    })
+                    if (!inner.some(checkFalsy)) {
+                        exit = true;
+                        gameWon = true;
+                    }
+                }
+            });
+        }
+        let allDirs = arr.concat(getCols, getDiags)
+        findWin(allDirs)
+        return gameWon
+    };
+
+    return {main, turn, getSelection, resetGame, checkWin}
 })();
+
+const Display = (function() {
+    const rows = Array.from(document.getElementsByClassName('container')),
+        cells = Array.from(document.getElementsByClassName('cell'))
+
+    const displayBoard = function(j, k, mark) {
+        if (!Game.checkWin(Board.boardArray)) {
+            Display.rows.forEach((row) => {
+                if (row.id == j) {
+                    Display.cells.forEach((cell) => {
+                        if (cell.id == k) {
+                            cell.textContent = mark;
+                        }
+                    })
+                }
+            })
+        }
+    }
+
+    return {rows, cells, displayBoard}
+})()
